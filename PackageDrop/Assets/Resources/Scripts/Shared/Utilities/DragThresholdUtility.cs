a9fe98d6923be5947921c13bf200ad36bf457abf
pientknk <pientagon@gmail.com>
2019-04-15 16:59:19 -0500

    reorganize and rename files

---
+++ PackageDrop/Assets/Resources/Scripts/Shared/Utilities/DragThresholdUtility.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragThresh : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int defaultValue = EventSystem.current.pixelDragThreshold;
+        EventSystem.current.pixelDragThreshold =
+                Mathf.Max(
+                     defaultValue,
+                     (int)(defaultValue * Screen.dpi / 160f));
+    }
+}
